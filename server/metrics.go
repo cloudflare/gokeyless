94e988c890dab62bcb7e5710380d1e9df7d26cd8
Jacob H. Haven <jacob@cloudflare.com>
2016-02-23 16:13:05 -0800

    Replace go-metrics with prometheus

--- server/metrics.go
+++ server/metrics.go
@@ -1,33 +1,31 @@
 package server
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/cloudflare/cfssl/log"
-	"github.com/cloudflare/go-metrics"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type statistics struct {
-	rate        metrics.Meter
-	invalidRate metrics.Meter
-	latency     metrics.Timer
-	metrics.Registry
+	requests        prometheus.Summary
+	requestsInvalid prometheus.Counter
 }
 
 func newStatistics(metricsAddr string) *statistics {
 	stats := &statistics{
-		rate:        metrics.NewMeter(),
-		invalidRate: metrics.NewMeter(),
-		latency:     metrics.NewTimer(),
-		Registry:    metrics.NewRegistry(),
+		requests: prometheus.NewSummary(prometheus.SummaryOpts{
+			Name: "requests",
+			Help: "Curent latency in responding to requests.",
+		}),
+		requestsInvalid: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "requests_invalid",
+			Help: "Number of invalid requests.",
+		}),
 	}
-	stats.Register("request_rate", stats.rate)
-	stats.Register("invalid_request_rate", stats.invalidRate)
-	stats.Register("response_latency", stats.latency)
+	prometheus.MustRegister(stats.requests)
+	prometheus.MustRegister(stats.requestsInvalid)
 
 	if metricsAddr != "" {
 		go stats.ListenAndServe(metricsAddr)
@@ -38,45 +36,17 @@ func newStatistics(metricsAddr string) *statistics {
 
 // logInvalid increments the error count and updates the error percentage.
 func (stats *statistics) logInvalid(requestBegin time.Time) {
-	stats.invalidRate.Mark(1)
+	stats.requestsInvalid.Inc()
 	stats.logRequest(requestBegin)
 }
 
 // logRequest increments the request count and updates the error percentage.
 func (stats *statistics) logRequest(requestBegin time.Time) {
-	stats.latency.UpdateSince(requestBegin)
-	stats.rate.Mark(1)
-}
-
-func trueString(s string) bool {
-	return s == "1" || strings.ToLower(s) == "true"
-}
-
-func (stats *statistics) serveJSON(w http.ResponseWriter, req *http.Request) {
-	js, err := json.Marshal(stats.Registry)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	buf := bytes.NewBuffer(js)
-	if trueString(req.URL.Query().Get("indent")) {
-		buf = bytes.NewBuffer(nil)
-		err = json.Indent(buf, js, "", "\t")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
-	w.Header().Set("Content-Type", "application/json")
-	content := bytes.NewReader(buf.Bytes())
-	http.ServeContent(w, req, "metrics.json", time.Now(), content)
+	stats.requests.Observe(float64(time.Now().Sub(requestBegin)))
 }
 
 func (stats *statistics) ListenAndServe(addr string) {
-	http.HandleFunc("/metrics", stats.serveJSON)
-	http.HandleFunc("/metrics.js", stats.serveJSON)
-	http.HandleFunc("/metrics.json", stats.serveJSON)
+	http.Handle("/metrics", prometheus.Handler())
 
 	log.Infof("Serving metrics endpoint at %s/metrics\n", addr)
 	log.Critical(http.ListenAndServe(addr, nil))
