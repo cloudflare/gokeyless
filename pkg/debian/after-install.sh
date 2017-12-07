4452ae3d68d2a01452235b214c5b384acdc4335d
Joshua Liebow-Feeser <hello@joshlf.com>
2017-12-06 16:58:33 -0800

    pkg: Don't configure during package installation

--- pkg/debian/after-install.sh
+++ pkg/debian/after-install.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
 chown -R keyless:keyless /etc/keyless
-
-cd /etc/keyless
-# Runs interactive auto-configuration (via CF API), but only if not already
-# configured.
-/usr/local/bin/gokeyless -config-only
