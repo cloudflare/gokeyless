19dbbe0b22e2835e496ac7a420a39eef270f5af2
Christopher Broglie <cbroglie@cloudflare.com>
2018-02-21 17:45:41 -0800

    Restart the running service when the package is ugpraded

--- pkg/centos/after-install.sh
+++ pkg/centos/after-install.sh
@@ -1,3 +1,8 @@
 /sbin/chkconfig --add gokeyless
 
 chown -R keyless:keyless /etc/keyless
+
+# restart the service iff it was already running
+if /sbin/service gokeyless status >/dev/null 2>&1 ; then
+  /sbin/service gokeyless restart >/dev/null 2>&1 || true
+fi
