3d71a314cf54d17d92d6ec9605debd3042cec52a
Christopher Broglie <cbroglie@cloudflare.com>
2018-02-21 17:45:41 -0800

    Restart the running service when the package is ugpraded

--- pkg/debian/after-install.sh
+++ pkg/debian/after-install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 chown -R keyless:keyless /etc/keyless
+
+# restart the service iff it was already running
+if service gokeyless status >/dev/null 2>&1 ; then
+  service gokeyless restart >/dev/null 2>&1 || true
+fi
